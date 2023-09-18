c32e77d9459777bf9d90ce3c0423edf1f567e94a
bjugoy <richardtlibres@gmail.com>
2023-09-18 16:00:22 +0200

    Initial commit

---
+++ files/main.js
@@ -0,0 +1,300 @@
+/* A builder class to simplify the task of creating HTML elements */
+class ElementCreator {
+    constructor(tag) {
+        this.element = document.createElement(tag);
+    }
+
+    id(id) {
+        this.element.id = id;
+        return this;
+    }
+
+    class(clazz) {
+        this.element.class = clazz;
+        return this;
+    }
+
+    text(content) {
+        this.element.innerHTML = content;
+        return this;
+    }
+
+    with(name, value) {
+        this.element.setAttribute(name, value)
+        return this;
+    }
+
+    listener(name, listener) {
+        this.element.addEventListener(name, listener)
+        return this;
+    }
+
+    append(child) {
+        child.appendTo(this.element);
+        return this;
+    }
+
+    prependTo(parent) {
+        parent.prepend(this.element);
+        return this.element;
+    }
+
+    appendTo(parent) {
+        parent.append(this.element);
+        return this.element;
+    }
+
+    insertBefore(parent, sibling) {
+        parent.insertBefore(this.element, sibling);
+        return this.element;
+    }
+
+    replace(parent, sibling) {
+        parent.replaceChild(this.element, sibling);
+        return this.element;
+    }
+}
+
+/* A class representing a resource. This class is used per default when receiving the
+   available resources from the server (see end of this file).
+   You can (and probably should) rename this class to match with whatever name you
+   used for your resource on the server-side.
+ */
+class Book {
+
+    /* If you want to know more about this form of getters, read this:
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get */
+    get idforDOM() {
+        return `resource-${this.id}`;
+    }
+
+}
+
+function add(resource, sibling) {
+
+    const creator = new ElementCreator("article")
+        .id(resource.idforDOM);
+
+    /* Task 2: Instead of the name property of the example resource, add the properties of
+       your resource to the DOM. If you do not have the name property in your resource,
+       start by removing the h2 element that currently represents the name. For the 
+       properties of your object you can use whatever html element you feel represents
+       your data best, e.g., h2, paragraphs, spans, ... 
+       Also, you don't have to use the ElementCreator if you don't want to and add the
+       elements manually. */
+
+    creator
+        .append(new ElementCreator("h2").text(resource.title))
+        .append(new ElementCreator("p").text(resource.author))
+        .append(new ElementCreator("p").text(resource.isbn))
+        .append(new ElementCreator("p").text("In Stock: " + resource.inStock))
+        .append(new ElementCreator("p").text(resource.releaseDate))
+
+    creator
+        .append(new ElementCreator("button").text("Edit").listener('click', () => {
+            edit(resource);
+        }))
+        .append(new ElementCreator("button").text("Remove").listener('click', () => {
+            /* Task 3: Call the delete endpoint asynchronously using either an XMLHttpRequest
+               or the Fetch API. Once the call returns successfully, remove the resource from
+               the DOM using the call to remove(...) below. */
+            remove(resource);  // <- This call removes the resource from the DOM. Call it after (and only if) your API call succeeds!
+        }));
+
+    const parent = document.querySelector('main');
+
+    if (sibling) {
+        creator.replace(parent, sibling);
+    } else {
+        creator.insertBefore(parent, document.querySelector('#bottom'));
+    }
+        
+}
+
+function edit(resource) {
+    const formCreator = new ElementCreator("form")
+        .id(resource.idforDOM)
+        .append(new ElementCreator("h3").text("Edit " + resource.title));
+    
+    /* Task 4 - Part 1: Instead of the name property, add the properties your resource has here!
+       The label and input element used here are just an example of how you can edit a
+       property of a resource, in the case of our example property name this is a label and an
+       input field. Also, we assign the input field a unique id attribute to be able to identify
+       it easily later when the user saves the edited data (see Task 4 - Part 2 below). 
+    */
+    formCreator
+        .append(new ElementCreator("label").text("Title").with("for", "resource-title"))
+        .append(new ElementCreator("input").id("resource-title").with("type", "text").with("value", resource.title))
+        .append(new ElementCreator("label").text("Author").with("for", "resource-author"))
+        .append(new ElementCreator("input").id("resource-author").with("type", "text").with("value", resource.author))
+        .append(new ElementCreator("label").text("ISBN").with("for", "resource-isbn"))
+        .append(new ElementCreator("input").id("resource-isbn").with("type", "text").with("value", resource.isbn))
+        .append(new ElementCreator("label").text("In Stock").with("for", "resource-stock"))
+        .append(new ElementCreator("input").id("resource-stock").with("type", "checkbox").with("checked", resource.inStock))
+        .append(new ElementCreator("label").text("Release Date").with("for", "resource-date"))
+        .append(new ElementCreator("input").id("resource-date").with("type", "date").with("value", resource.releaseDate));
+
+    /* In the end, we add the code to handle saving the resource on the server and terminating edit mode */
+    formCreator
+        .append(new ElementCreator("button").text("Speichern").listener('click', (event) => {
+            /* Why do we have to prevent the default action? Try commenting this line. */
+            event.preventDefault();
+
+            /* The user saves the resource.
+               Task 4 - Part 2: We manually set the edited values from the input elements to the resource object. 
+               Again, this code here is just an example of how the name of our example resource can be obtained
+               and set in to the resource. The idea is that you handle your own properties here.
+            */
+            fetch(`/api/resources/${resource.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(resource), // Send the updated resource as JSON
+            })
+                .then(response => {
+                    if (response.ok) {
+                        // Resource successfully updated on the server
+                        console.log('Resource updated successfully.');
+                        /* Task 4 - Part 4: After the PUT call returns, remove the form and re-render the resource */
+
+                        const editForm = document.getElementById(resource.idforDOM);
+                        editForm.remove();
+
+                        add(resource, document.getElementById(resource.idforDOM));
+                    } else {
+                        throw new Error('Failed to update resource.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error updating resource:', error);
+                });
+        }))
+        .replace(document.querySelector('main'), document.getElementById(resource.idforDOM));
+}
+
+function remove(resource) {
+    document.getElementById(resource.idforDOM).remove();
+
+    fetch(`/api/resources/${resource.id}`, {
+        method: 'DELETE',
+    })
+        .then(response => {
+            if (response.ok) {
+                // Resource successfully deleted from the server
+                console.log('Resource deleted successfully.');
+            } else {
+                throw new Error('Failed to delete resource.');
+            }
+        })
+        .catch(error => {
+            console.error('Error deleting resource:', error);
+        });
+}
+
+/* Task 5 - Create a new resource is very similar to updating a resource. First, you add
+   an empty form to the DOM with the exact same fields you used to edit a resource.
+   Instead of PUTing the resource to the server, you POST it and add the resource that
+   the server returns to the DOM (Remember, the resource returned by the server is the
+    one that contains an id).
+ */
+function create() {
+    // Check if a create form container is already visible
+    const existingFormContainer = document.querySelector('main form');
+    if (existingFormContainer) {
+        return;
+    }
+
+    const formCreator = new ElementCreator("form");
+
+    formCreator.append(new ElementCreator("h3").text("Create New Book"));
+
+
+    const createFormField = (labelText, id, type) => {
+        return new ElementCreator("label").text(labelText).with("for", id)
+            .append(new ElementCreator("input").id(id).with("type", type));
+    };
+
+    formCreator
+        .append(createFormField("Title", "new-resource-title", "text"))
+        .append(createFormField("Author", "new-resource-author", "text"))
+        .append(createFormField("ISBN", "new-resource-isbn", "text"))
+        .append(createFormField("In Stock", "new-resource-stock", "checkbox"))
+        .append(createFormField("Release Date", "new-resource-date", "date"));
+
+    formCreator
+        .append(new ElementCreator("button").text("Create").listener('click', (event) => {
+            event.preventDefault();
+
+
+            const newResource = {
+                title: document.getElementById("new-resource-title").value,
+                author: document.getElementById("new-resource-author").value,
+                isbn: document.getElementById("new-resource-isbn").value,
+                inStock: document.getElementById("new-resource-stock").checked,
+                releaseDate: document.getElementById("new-resource-date").value,
+            };
+
+
+            fetch("/api/resources", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newResource),
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Failed to create resource.');
+                    }
+                })
+                .then(createdResource => {
+
+                    add(Object.assign(new Book(), createdResource));
+
+                    const formContainer = document.querySelector('main form');
+                    if (formContainer) {
+                        formContainer.remove();
+                    }
+                })
+                .catch(error => {
+                    console.error('Error creating resource:', error);
+                })
+                .finally(() => {
+                    clearFormFields();
+                });
+        }))
+        .append(new ElementCreator("button").text("Cancel").listener('click', (event) => {
+        event.preventDefault();
+
+        // Remove the form container when "Cancel" is clicked
+        const formContainer = document.querySelector('main form');
+        if (formContainer) {
+            formContainer.remove();
+        }
+    }));
+
+    formCreator.prependTo(document.querySelector('main'));
+
+    function clearFormFields() {
+        document.getElementById("new-resource-title").value = '';
+        document.getElementById("new-resource-author").value = '';
+        document.getElementById("new-resource-isbn").value = '';
+        document.getElementById("new-resource-stock").checked = false;
+        document.getElementById("new-resource-date").value = '';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    fetch("/api/resources")
+        .then(response => response.json())
+        .then(resources => {
+            for (const resource of resources) {
+                add(Object.assign(new Book(), resource));
+            }
+        });
+});
+
